d9ba58b25a17a22cc9ffbddf49cd83108c172a7f
Gautam Soni <your_github_email@example.com>
2026-03-20 17:52:58 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://api-authentication-y2my.vercel.app",
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
